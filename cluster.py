e070c4df3f0c23758922761c226a7739f2a064f7
haebin.lee <luckylucy0717@gmail.com>
2024-02-21 10:33:08 -0800

    add balanced option

--- cluster.py
+++ cluster.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 import random
-
+import heapq
 
 class cluster:
     def __init__(self, k=5, max_iterations=100):
@@ -11,21 +11,52 @@ class cluster:
         self._centroids = np.random.rand(k, 2)
 
     def closest(self, p):
-        min = sys.maxsize
+        min_v = sys.maxsize
         min_i = 0
         for i, center in enumerate(self._centroids):
             d = self.distance(p, center)
-            if min > d:
-                min = d
+            if min_v > d:
+                min_v = d
                 min_i = i
         return min_i
 
     def distance(self, x, y):
         return np.linalg.norm(x - y)
 
+    def balance(self, X, hypotheses):
+
+        for _ in range(5):
+            desired_size = round(len(X) / self._k)
+            eps = round(desired_size * 0.1)
+            count = np.bincount(hypotheses, minlength=self._k)
+            max_count = max(count)
+            min_count = min(count)
+            max_idx = np.argmax(count)
+            min_idx = np.argmin(count)
+
+            if max_count <= desired_size + eps or desired_size - eps <= min_count:
+                break
+
+            pq = []
+            need_to_update = np.where(hypotheses != min_idx)[0]
+            for i in need_to_update:
+                dist = self.distance(X[i], self._centroids[min_idx])
+                pq.append((dist, i))
+            heapq.heapify(pq)
+
+            while min_count < desired_size - eps and pq:
+                _, idx = heapq.heappop(pq)
+                hypotheses[idx] = min_idx
+                min_count += 1
+
+            for i in range(self._k):
+                cluster_points = X[hypotheses == i]
+                if len(cluster_points) > 0:
+                    self._centroids[i] = np.mean(cluster_points, axis=0)
+        return hypotheses
+
     def fit(self, X, balanced=False):
         # X: 2D array
-        self._centroids = random.choices(X, k=self._k)
 
         hypotheses = np.full(len(X), -1)
         for _ in range(self._max_iterations):
@@ -45,18 +76,16 @@ class cluster:
 
             hypotheses = hypotheses.astype(int)
 
-            # if balanced:
-            #     hypotheses = self.balance(X, hypotheses)
-
             if np.array_equal(prev_centroids, self._centroids):
                 break
-
+        if balanced:
+            hypotheses = self.balance(X, hypotheses)
         # A: A list (of length n) of the cluster hypotheses, one for each instances
         # B: A list (of length at most k) containing lists (each of length d) of the cluster centroids' values.
         return hypotheses, self._centroids
 
 
-# t = cluster(2)
+# t = cluster(5)
 # X = np.array([[0, 0], [2, 2], [0, 2], [2, 0], [10, 10], [8, 8], [10, 8], [8, 10]])
 # h, centroids = t.fit(X, True)
 # print(h, centroids)
